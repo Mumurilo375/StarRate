44d7ffc43ad5fb31e1c817713132d611d8ddcae4
Mumurilo375 <murilinho.pequeno@gmail.com>
2025-08-27 00:13:21 -0300

    Primeiro commit, fiz o header, falta instalar as bibliotecas do composer

---
+++ doctrine-master/public/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/searchBar.css">
+
+    <title>StarRate - Avalie filmes</title>
+</head>
+<body>
+
+
+
+    <header>
+        <nav class="navbabrHeader">
+            <ol class="olDoHeader">
+                <li><a href="index.php"><img class="logo" src="img/Logo.png" alt="StarRate"></a>
+                <div id="opcoesHeader">
+                    <li><a href="index.php"><h1 id="starRate">StarRate</h1></a></li>
+                    <li><a href=""><h1 id="singin">ENTRAR</h1></a>
+                    <li><a href=""><h1 id="createAccount">CRIAR CONTA</h1></a>
+                    <li><a href=""><h1 id="films">FILMES</h1></a>
+                </div>
+            </ol>
+            <form class="search-container">
+
+                <input type="text" class="search-input" placeholder="Pesquisar...">
+
+                <button type="submit" class="search-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+                    </svg>
+                </button>
+            </form>
+        </nav>
+    </header>
+    
+    <div>
+        <img class="fight" src="img/fight.jp" alt="">
+    </div>
+    
+    
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
+    <header>
+        <nav class="navbar" style="background-color: #e3f2fd;" data-bs-theme="light">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div class="navbar-nav">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link" href="#">Features</a>
+                                <a class="nav-link" href="#">Pricing</a>
+                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                            </div>
+                        </div>
+                </div>
+            </nav>
+        </nav>
+    </header>
+    -->
